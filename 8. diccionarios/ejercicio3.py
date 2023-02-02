47be3266887496a8581ed76490d29a58806a417e
Alejandro Mellado <alejandromm@vauxoo.com>
2023-02-02 16:21:28 -0700

    capitulo 8: agregar solucion de ejercicios 3, 4, 5

---
+++ 8. diccionarios/ejercicio3.py
@@ -0,0 +1,31 @@
+# Ejercicio 3: Escribe un programa para leer a través de un historial de
+# correos, construye un histograma utilizando un diccionario para contar
+# cuántos mensajes han llegado de cada dirección de correo electrónico, e
+# imprime el diccionario.
+# Ingresa un nombre de archivo: mbox-short.txt
+# {'gopal.ramasammycook@gmail.com': 1, 'louis@media.berkeley.edu': 3,
+# 'cwen@iupui.edu': 5, 'antranig@caret.cam.ac.uk': 1,
+# 'rjlowe@iupui.edu': 2, 'gsilver@umich.edu': 3,
+# 'david.horwitz@uct.ac.za': 4, 'wagnermr@iupui.edu': 1,
+# 'zqian@umich.edu': 4, 'stephen.marquard@uct.ac.za': 2,
+# 'ray@media.berkeley.edu': 1}
+
+def main():
+    filename = input('Ingresar nombre de archivo: ')
+    try:
+        with open(filename) as file:
+            relevant_lines = [line for line in file if 'From ' in line]
+
+            email_counter = {}
+            for line in relevant_lines:
+                email = line.split()[1]
+                email_counter[email] = 1 if (email not in email_counter) else (email_counter[email] + 1)
+
+            print(email_counter)
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    main()
