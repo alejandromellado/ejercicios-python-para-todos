4ec58b3996ff19dacd0c4bdbcaac82abecf9b85a
Alejandro Mellado <alejandromm@vauxoo.com>
2023-02-03 14:13:29 -0700

    capitulo 9: crear archivo ejercicio2.py

---
+++ 9. tuplas/ejercicio3.py
@@ -0,0 +1,15 @@
+# Ejercicio 3: Escribe un programa que lee un archivo e imprime las
+# letras en order decreciente de frecuencia. El programa debe convertir
+# todas las entradas a minúsculas y contar solamente las letras a-z.
+# El programa no debe contar espacios, dígitos, signos de puntuación,
+# o cualquier cosa que no sean las letras a-z. Encuentra ejemplos de
+# texto en idiomas diferentes, y observa cómo la frecuencia de letras es
+# diferente en cada idioma. Compara tus resultados con las tablas en
+# https://es.wikipedia.org/wiki/Frecuencia_de_aparici%C3%B3n_de_letras.
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
